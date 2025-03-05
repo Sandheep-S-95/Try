6812a40c81e0e60bc2f51e0e7f5d0ac6d7535fc8
Sandheep S <138574897+Sandheep-S-95@users.noreply.github.com>
2025-03-05 20:28:07 +0530

    Delete CA2 directory

--- CA2/Database/db.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-conn=sqlite3.connect("./CA2/Database/weather")
-cur=conn.cursor()
-cur.execute("CREATE TABLE userCities(id INTEGER PRIMARY KEY AUTOINCREMENT,city VARCHAR NOT NULL,latitude FLOAT NOT NULL,longitude FLOAT NOT NULL,temp FLOAT NOT NULL,max_temp FLOAT NOT NULL,min_temp FLOAT NOT NULL)")
-conn.commit()
-conn.close()
